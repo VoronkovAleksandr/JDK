ebb422a690893e924e090f1b64c9415ee755c1d9
VoronkovAleksandr <a_voron@list.ru>
2023-12-20 21:48:10 +0500

    HomeWork005 completed

--- HomeWork005/src/main/java/Main.java
+++ HomeWork005/src/main/java/Main.java
@@ -1,2 +1,14 @@
+import java.util.ArrayList;
+
 public class Main {
+    public static void main(String[] args) {
+        ArrayList<Thread> wiseMen = new ArrayList<>();
+        wiseMen.add(new Thread(WiseMan::new));
+        wiseMen.add(new Thread(WiseMan::new));
+        wiseMen.add(new Thread(WiseMan::new));
+        wiseMen.add(new Thread(WiseMan::new));
+        wiseMen.add(new Thread(WiseMan::new));
+        wiseMen.forEach(Thread::start);
+    }
+
 }
--- HomeWork005/src/main/java/WiseMan.java
+++ HomeWork005/src/main/java/WiseMan.java
@@ -1,2 +1,37 @@
+import java.util.Random;
+
 public class WiseMan {
+    private final String name;
+    private final Integer thinkTime;
+    private final Integer EAT_TIME = 500;
+
+    public WiseMan() {
+        name = Thread.currentThread().getName();
+        thinkTime = new Random().nextInt(2000);
+        for (int i = 0; i < 3; i++) {
+            eat();
+            think();
+        }
+    }
+
+    public void eat() {
+        System.out.printf("Мудрец %s кушает! \n", name);
+        try {
+            Thread.sleep(EAT_TIME);
+        } catch (InterruptedException e) {
+            System.out.printf("Ошибка во время обеда мудреца %s", name);
+        }
+    }
+
+    public void think(){
+        int thinkPeriod = thinkTime/10;
+        for (int i = 0; i < 10; i++) {
+            System.out.printf("Мудрец %s размышляет ... \n", name);
+            try {
+                Thread.sleep(thinkPeriod);
+            } catch (InterruptedException e) {
+                System.out.printf("Ошибка во время размышлений мудреца %s", name);
+            }
+        }
+    }
 }
